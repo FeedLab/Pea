813f28f6f9f389890238ca6a147aadf80378d68b
Jonny Nilsson <jompa67@gmail.com>
2026-02-26 12:13:05 +0700

    tab style

--- Pea.Meter/ViewModel/PeaServicesViewModel.cs
+++ Pea.Meter/ViewModel/PeaServicesViewModel.cs
@@ -1,22 +1,35 @@
-﻿using CommunityToolkit.Mvvm.Input;
+﻿using CommunityToolkit.Maui;
+using CommunityToolkit.Mvvm.Input;
 using CommunityToolkit.Mvvm.Messaging;
 using Pea.Meter.Models;
+using Pea.Meter.Services;
 
 namespace Pea.Meter.ViewModel;
 
 public partial class PeaServicesViewModel
 {
+    private readonly IPopupService popupService;
+
+    public PeaServicesViewModel(IPopupService popupService)
+    {
+        this.popupService = popupService;
+    }
+
     [RelayCommand]
     private async Task OpenUrl(string url)
     {
         try
         {
-            await Browser.Default.OpenAsync(url, BrowserLaunchMode.External);
+            if (string.IsNullOrWhiteSpace(url))
+                return;
+
+            Uri uri = new Uri(url);
+            await Browser.Default.OpenAsync(uri, BrowserLaunchMode.SystemPreferred);
         }
         catch (Exception ex)
         {
-            // Handle exception if needed
             System.Diagnostics.Debug.WriteLine($"Unable to open URL: {ex.Message}");
+            //await Application.Current.MainPage.DisplayAlert("Error", $"Unable to open URL: {ex.Message}", "OK");
         }
     }
 }
