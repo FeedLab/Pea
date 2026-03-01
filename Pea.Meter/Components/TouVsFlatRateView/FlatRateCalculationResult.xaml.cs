2e97591982e41d8c671c1d126644c8594d4d4b71
Jonny Nilsson <jompa67@gmail.com>
2026-03-01 14:49:27 +0700

    Created component from existin xaml

---
+++ Pea.Meter/Components/TouVsFlatRateView/FlatRateCalculationResult.xaml.cs
@@ -0,0 +1,9 @@
+namespace Pea.Meter.Components.TouVsFlatRateView;
+
+public partial class FlatRateCalculationResult : ContentView
+{
+	public FlatRateCalculationResult()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Pea.Meter/Components/TouVsFlatRateView/TouCalculationResult.xaml.cs
@@ -0,0 +1,9 @@
+namespace Pea.Meter.Components.TouVsFlatRateView;
+
+public partial class TouCalculationResult : ContentView
+{
+	public TouCalculationResult()
+	{
+		InitializeComponent();
+	}
+}
