cc24d121d30e8970a7402632f9e548b8503f980c
Jonny Nilsson <jompa67@gmail.com>
2026-03-02 10:17:22 +0700

    components of statistic

---
+++ Pea.Meter/View/Components/MeterComparisonChart.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections.ObjectModel;
+using Pea.Infrastructure.Models;
+
+namespace Pea.Meter.View.Components;
+
+public partial class MeterComparisonChart : ContentView
+{
+    public static readonly BindableProperty ChartTitleProperty =
+        BindableProperty.Create(nameof(ChartTitle), typeof(string), typeof(MeterComparisonChart), string.Empty);
+
+    public static readonly BindableProperty TodayDataProperty =
+        BindableProperty.Create(nameof(TodayData), typeof(ObservableCollection<PeaMeterReading>), typeof(MeterComparisonChart), null);
+
+    public static readonly BindableProperty Average1DayDataProperty =
+        BindableProperty.Create(nameof(Average1DayData), typeof(ObservableCollection<PeaMeterReading>), typeof(MeterComparisonChart), null);
+
+    public static readonly BindableProperty Average7DaysDataProperty =
+        BindableProperty.Create(nameof(Average7DaysData), typeof(ObservableCollection<PeaMeterReading>), typeof(MeterComparisonChart), null);
+
+    public static readonly BindableProperty Average30DaysDataProperty =
+        BindableProperty.Create(nameof(Average30DaysData), typeof(ObservableCollection<PeaMeterReading>), typeof(MeterComparisonChart), null);
+
+    public string ChartTitle
+    {
+        get => (string)GetValue(ChartTitleProperty);
+        set => SetValue(ChartTitleProperty, value);
+    }
+
+    public ObservableCollection<PeaMeterReading> TodayData
+    {
+        get => (ObservableCollection<PeaMeterReading>)GetValue(TodayDataProperty);
+        set => SetValue(TodayDataProperty, value);
+    }
+
+    public ObservableCollection<PeaMeterReading> Average1DayData
+    {
+        get => (ObservableCollection<PeaMeterReading>)GetValue(Average1DayDataProperty);
+        set => SetValue(Average1DayDataProperty, value);
+    }
+
+    public ObservableCollection<PeaMeterReading> Average7DaysData
+    {
+        get => (ObservableCollection<PeaMeterReading>)GetValue(Average7DaysDataProperty);
+        set => SetValue(Average7DaysDataProperty, value);
+    }
+
+    public ObservableCollection<PeaMeterReading> Average30DaysData
+    {
+        get => (ObservableCollection<PeaMeterReading>)GetValue(Average30DaysDataProperty);
+        set => SetValue(Average30DaysDataProperty, value);
+    }
+
+    public MeterComparisonChart()
+    {
+        InitializeComponent();
+    }
+}
