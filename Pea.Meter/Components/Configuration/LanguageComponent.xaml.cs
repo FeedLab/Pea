0cfb5cb1e657d45ed68ea8794a1d91246da490a3
Jonny Nilsson <jompa67@gmail.com>
2026-03-22 21:00:15 +0700

    config with slider test

---
+++ Pea.Meter/Components/Configuration/LanguageComponent.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pea.Meter.Components.Configuration;
+
+public partial class LanguageComponent : ContentView
+{
+    public LanguageComponent()
+    {
+        InitializeComponent();
+    }
+}
